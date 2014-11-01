314b301a0ab4b7a35156d8150724d4dbc1a2f58d
mrkoal <mrkoal@gmail.com>
2014-11-02 00:40:42 +0800

    0
    0

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
 	
 	
 	jQuery('#nav-button').click(function() {
-			jQuery('#options').toggle(1000);
+			jQuery('#options').toggle();
 	});
 	
 	if ( jQuery(window).width() < 959) {
