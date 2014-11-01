c0a28a49701283ccd5b531b0040c4a1fc875a2d8
mrkoal <mrkoal@gmail.com>
2014-11-01 23:25:32 +0800

    Revert "Revert "add""
    This reverts commit 0e96fffab2ec3852928a74e1882d4b8f52d4e242.

---
+++ js/image-hover.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function(){
+ 
+	 $('.title-wrap, .subtitle-wrap').each(function() {
+        $(this).data('wrapping', $(this).width());
+        $(this).css('width', 0);
+      });
+
+      $('div.images').bind('mouseenter', function() {
+        $(this).find('.title-wrap').stop().each(function() {
+          $(this).animate({
+            width: $(this).data('wrapping')
+          }, 400);
+        });
+
+        $(this).find('.subtitle-wrap').stop().each(function() {
+          $(this).delay(200).animate({
+            width: $(this).data('wrapping')
+          }, 400);
+        });
+      });
+
+      $('div.images').bind('mouseleave', function() {
+        $(this).delay(200).find('.title-wrap').stop().each(function() {
+          $(this).animate({
+            width: 0
+          }, 400);
+        });
+
+        $(this).find('.subtitle-wrap').stop().each(function() {
+          $(this).animate({
+            width: 0
+          }, 400);
+        });
+      });
+  $('.element').hover(function(){
+    $('.element').addClass('nohover');
+    $(this).addClass('hover').removeClass('nohover');
+  },function(){
+    $('.element').removeClass('nohover').removeClass('hover');
+  });
+});
